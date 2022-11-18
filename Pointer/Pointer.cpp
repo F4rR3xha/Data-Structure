4d5b703d9d6c124e3a29e3d37b5aefae99d5f33a
F4rR3xha <ikhwanfilaw22@gmail.com>
2022-11-18 14:19:14 +0700

    Pointer

---
+++ Pointer/Pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Mencetak alamat pada variable
+    string nama = "Ikhwan Fi'la Wadzakiiya";
+    cout << nama << " berada pada alamat\t: " << &nama << endl;
+
+    // Mendeklarasi Variable Pointer
+    string *pointerNama = &nama;
+    cout << "Pointer Nama\t\t\t\t\t: " << pointerNama << endl;
+
+    // Mencetak nilai pada variable pointer
+    cout << "Nilai pada pointer nama\t\t\t\t: " << *pointerNama << endl;
+
+    // Manipulasi nilai
+    *pointerNama = "Fara Darniva";
+    cout << "Nilai pada pointer nama\t\t\t\t: " << *pointerNama << endl;
+
+}
